0fb60891244e174a874d9295918e791b9f61b30c
Sala3ni <salonimishramishra9527@gmail.com>
2025-08-15 15:42:26 +0530

    updated backend URL

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ const API_BASE_URL =
   import.meta.env.VITE_API_BASE_URL || // Vite env variable
   (import.meta.env.MODE === "development"
     ? "http://localhost:5000"
-    : "https://ordermanagement-backend-a82l71vqz-saloni-mishras-projects.vercel.app");
+    : "https://ordermanagement-backend-e9ohmfxa5-saloni-mishras-projects.vercel.app");
 
 function App() {
   const [orders, setOrders] = useState([]);
